1c6f06e647a14e2596c128b6657a507701f51d20
jbuzzanco <jcbuzzanco@gmail.com>
2017-10-23 18:38:21 -0400

    Begins adding map service
    will need to finish writing map service as well as add
    defaultCoords, defaultZoom to html at some point for this to work

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { MapService } from '../services/map.service';
+import { IActivity} from '../../shared/activity.model';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _mapService: MapService,
+              private _route: ActivatedRoute) { }
+
+  activity: any;
+  activityName: string;
+  activityComments: string;
+  activityDate: Date;
+  activiytDistance: number;
+  gpx: any;
+
 
   ngOnInit() {
+    this.activity = this._mapService.getActivity(
+      +this._route.snapshot.params['id']
+    )
   }
 
 }
---
+++ src/app/services/map.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { IActivity } from '../../shared/activity.model';
+import { ActivityService } from '../services/activity.service';
+import { SAVED_ACTIVITIES } from '../../shared/activities';
+
+let apitoken = environment.MAPBOX_API_KEY;
+declare var omnivore: any;
+declare var L: any;
+
+const defaultCoords: number[] = [40, -80];
+const defaultZoom: number = 8;
+
+
+@Injectable()
+export class MapService {
+
+  getActivity(id: number){
+    return SAVED_ACTIVITIES.slice(0).find(run => run.id == id)
+  }
+
+  plotActivity(id: number){
+    var myStyle = {
+      'color': '#3949AB',
+      'weight': 5,
+      'opacity': 0.95
+    }
+
+    var map = L.map('map').setView(defaultCoords, defaultZoom);
+
+    map.maxZoom = 100;
+
+    L.tileLayer('https://api.mapbox.com/v4{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets-basic',
+      accessToken: apiToken
+    }).addTo(map)
+  }
+
+  constructor() { }
+
+}
