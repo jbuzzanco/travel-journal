1efe339694980bcc07dac2c66f86bd8ee6810deb
jbuzzanco <jcbuzzanco@gmail.com>
2017-10-25 18:19:18 -0400

    Adds activities.ts with new path to gpx data
    assets/gpx instead of ../../assets/gps

--- src/shared/activities.ts
+++ src/shared/activities.ts
@@ -6,21 +6,21 @@ export const SAVED_ACTIVITIES: IActivity[] = [
     'name' : 'Presque Isle Park',
     'date' : new Date('10/06/2017'),
     'distance' : 6.3,
-    'gpxData' : '../../assets/gpx/1.gpx'
+    'gpxData' : 'assets/gpx/1.gpx'
   },
   {
     'id' : 2,
     'name' : 'North Chagrin Reservation',
     'date' : new Date('10/06/2017'),
     'distance' : 1.8,
-    'gpxData' : '../../assets/gpx/2.gpx'
+    'gpxData' : 'assets/gpx/2.gpx'
   },
   {
     'id' : 3,
     'name' : 'Valerie Falls Trail',
     'date' : new Date('10/05/2017'),
     'distance' : 1.7,
-    'gpxData' : '../../assets/gpx/2.gpx',
+    'gpxData' : 'assets/gpx/2.gpx',
     'comments' : 'warm and sunny'
   },
   {
@@ -28,7 +28,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
     'name' : 'Valerie Falls Trail',
     'date' : new Date('10/05/2017'),
     'distance' : 7,
-    'gpxData' : '../../assets/gpx/2.gpx',
+    'gpxData' : 'assets/gpx/4.gpx',
     'comments' : 'windy. Paved biking path'
   }
 ]
