5416044e2db853cde0108fcb48719b3f99e4c8d0
jbuzzanco <jcbuzzanco@gmail.com>
2017-10-24 20:56:55 -0400

    Adds environment files into gitignore
    I added these files in version control

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  MAPBOX_API_KEY: 'pk.eyJ1IjoiamJ1enphbmNvIiwiYSI6ImNqOTRxbTQ0cDQ4OXYycm1yc2UxN200eDMifQ.m8FiwONMLLvwuUjOQm6TqQ'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  MAPBOX_API_KEY: 'pk.eyJ1IjoiamJ1enphbmNvIiwiYSI6ImNqOTRxbTQ0cDQ4OXYycm1yc2UxN200eDMifQ.m8FiwONMLLvwuUjOQm6TqQ'
 };
